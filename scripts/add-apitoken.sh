8601fc494f4a7fc486bee62068d0bb11fb1b75a1
dalon1 <dannelvision@gmail.com>
2019-10-18 14:19:25 -0400

    fixing stuff

--- scripts/add-apitoken.sh
+++ scripts/add-apitoken.sh
@@ -11,8 +11,21 @@ echo "$(date)"
 echo '==============================='
 echo ''
 
+
+# Authenticating to retrieve Jenkins Crumb as part of authentication header
+echo "Authenticating using '${JENKINS_USER}' credentials."
+CRUMB=$(curl -s "${JENKINS_URL}/crumbIssuer/api/json" -u $USER:$USER_CRED | jq '.crumb')
+CRUMB=${CRUMB/'"'/''} # x2 to remove double quotes
+CRUMB=${CRUMB/'"'/''} # x2 to remove double quotes
+echo "Jenkins crumb received: ${CRUMB}"
+echo "Jenkins Authentication..............................OK"
+
+
+
 # Creating a new Jenkins API Token
-curl -u $USER:$USER_CRED \
+curl --include -u $USER:$USER_CRED \
+-H "Jenkins-Crumb: ${CRUMB}" \
+-H "Content-Type:application/x-www-form-urlencoded" \
 -d 'newTokenName=foo' \
 -X POST ${JENKINS_URL}/user/${USER}/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken
 
